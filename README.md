5660609deb5759a77ef0dd96c1d72383b2e6e49c
Joseph Quinn <quinn.josephr@gmail.com>
2018-09-14 09:20:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# nghttp2-rs
+
+## License
+
+This crate is licensed under the [ISC License](LICENSE.md).
+
+## Release Notes
+
+- v0.1.0 - Initial release.
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2018 Joseph R. Quinn <quinn.josephr@protonmail.com>
+// SPDX-License-Identifier: ISC
+
+extern crate nghttp2_sys;
+extern crate libc;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
